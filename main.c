d81418e3437cd8a5fe898f9818218006773e8a19
Subhajit Patra <1souvikpa@gmail.com>
2020-09-17 17:42:11 +0530

    My first commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is Subhajit Patra.\n");// the output will be "My name is Subhajit Patra."d
+    printf("I read in class 9.\n");
+    return 0;
+}
